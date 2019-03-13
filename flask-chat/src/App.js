e733ee35f5df0eb9b6263a012957628e57775174
mark182182 <versalife11@hotmail.com>
2019-03-13 15:39:51 +0100

    fix(renderMessageFromSocket): use forceUpdate to refresh the DOM

--- flask-chat/src/App.js
+++ flask-chat/src/App.js
@@ -20,16 +20,19 @@ class App extends Component {
 
   componentDidMount() {
     this.loadMessages();
-    const socket = socketIOClient('http://192.168.111.234:5000');
+    this.loadSocket();
+  }
+
+  loadSocket() {
+    const socket = socketIOClient('http://192.168.111.241:5000');
     socket.on('message', messageFromSocket => {
-      this.renderMessageFromSocket();
-      console.log(this.state.receivedMessages[19]);
-    })
+      this.renderMessageFromSocket(messageFromSocket);
+    });
   }
 
   loadMessages() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://192.168.111.234:5000/chat');
+    xhr.open('GET', 'http://192.168.111.241:5000/chat');
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4 && xhr.status === 200) {
         this.setState({
@@ -51,33 +54,33 @@ class App extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
-    fetch('http://192.168.111.234:5000/message', {
+    fetch('http://192.168.111.241:5000/message', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       mode: 'cors',
       body: JSON.stringify({ 'username': this.state.username, 'message': this.state.message })
     }).then(response => console.log(response));
-    this.renderMessageFromSocket();
   }
 
-  renderMessageFromSocket() {
+  renderMessageFromSocket(messageFromSocket) {
     this.setState({ update: 'updating' });
     this.setState(state => {
       for (let index = 0; index < state.receivedMessages.length - 1; index++) {
-        let currentUsername = state.receivedMessages[index][1];
+        const currentUsername = state.receivedMessages[index][1];
         state.receivedMessages[index][1] = state.receivedMessages[index + 1][1];
         state.receivedMessages[index + 1][1] = currentUsername;
-        let currentMessage = state.receivedMessages[index][2];
+        const currentMessage = state.receivedMessages[index][2];
         state.receivedMessages[index][2] = state.receivedMessages[index + 1][2];
         state.receivedMessages[index + 1][2] = currentMessage;
-        let currentTimestamp = state.receivedMessages[index][3];
+        const currentTimestamp = state.receivedMessages[index][3];
         state.receivedMessages[index][3] = state.receivedMessages[index + 1][3];
         state.receivedMessages[index + 1][3] = currentTimestamp;
       }
-      state.receivedMessages[19][1] = state.username;
-      state.receivedMessages[19][2] = state.message;
+      state.receivedMessages[19][1] = messageFromSocket.username;
+      state.receivedMessages[19][2] = messageFromSocket.message;
       state.receivedMessages[19][3] = new Date(Date.now()).toGMTString();
     });
+    this.forceUpdate();
     console.log(this.state.receivedMessages);
   }
 
